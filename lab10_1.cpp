03b1317867e53d0d2abacf44e1cb8ec3c8fd7270
bewzunjo <saran_kim@cmu.ac.th>
2021-12-14 15:48:19 +0700

    lab 10.1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include<iomanip>
 using namespace std;
 
 int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+	double loan;
+	double interest;
+	double payment;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	cout << "Enter initial loan: ";
+	cin >> loan;
+	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
+	cout << "Enter amount you can pay per year: ";
+	cin >> payment;
+
+	
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -18,16 +23,25 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+	for(int i = 1 ; loan > 0;i++){
+	    if (payment > loan){
+	    payment = loan + loan*(interest/100);
+	}
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << (loan*(interest/100));
+	cout << setw(13) << left << loan + (loan*(interest/100));
+	if (loan + loan*(interest/100) < 100){
+	payment = loan + loan*(interest/100);
+	cout << setw(13) << left << payment;    
+	}else{
+	cout << setw(13) << left << payment;
+	}
+	cout << setw(13) << left << loan + loan*(interest/100) - payment;
+	loan = loan + loan*(interest/100) - payment;
+	cout << "\n";	
+	}
 	return 0;
 }
