25a0d0264b94b4eb5fc14e375acfe3bd2f714c9b
bewzunjo <saran_kim@cmu.ac.th>
2021-12-14 15:47:10 +0700

    lab 10.2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+int printO (int n,int m){
+if(n<=0){
+	cout << "Invalid input" << "\n";}
+for (int i = 1; i <= n; i++){
+
+	for (int i = 1; i <= m; i++){
+		cout << "O";
+	}
+	if(m<=0){
+		cout << "Invalid input";break;}
+		cout << "\n";
+	}return 0;
+}
+
+
+
 
 int main(){
 	
