d7d6de19f7c62b1d0582e699666805f989f3fc8e
bewzunjo <saran_kim@cmu.ac.th>
2021-12-14 16:41:48 +0700

    lab 10.1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,47 +1,51 @@
+
 #include<iostream>
-#include<iomanip>
+#include<iomanip> 
 using namespace std;
 
-int main(){	
-	double loan;
+int main()
+{
+    double loan;
 	double interest;
-	double payment;
+	double payment; 
 
-	cout << "Enter initial loan: ";
-	cin >> loan;
-	cout << "Enter interest rate per year (%): ";
-	cin >> interest;
-	cout << "Enter amount you can pay per year: ";
-	cin >> payment;
+    cout << "Enter initial loan: "; 
+    cin >> loan;
+    cout << "Enter interest rate per year (%): "; 
+    cin >> interest;
+    cout << "Enter amount you can pay per year: ";
+    cin >> payment;
 
-	
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
-	cout << setw(13) << left << "Payment";
-	cout << setw(13) << left << "NewBalance";
-	cout << "\n";
-	
-	
-	for(int i = 1 ; loan > 0;i++){
-	    if (payment > loan){
-	    payment = loan + loan*(interest/100);
-	}
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << i; 
-	cout << setw(13) << left << loan;
-	cout << setw(13) << left << (loan*(interest/100));
-	cout << setw(13) << left << loan + (loan*(interest/100));
-	if (loan + loan*(interest/100) < 100){
-	payment = loan + loan*(interest/100);
-	cout << setw(13) << left << payment;    
-	}else{
-	cout << setw(13) << left << payment;
-	}
-	cout << setw(13) << left << loan + loan*(interest/100) - payment;
-	loan = loan + loan*(interest/100) - payment;
-	cout << "\n";	
-	}
-	return 0;
+    cout << setw(13) << left << "EndOfYear#"; 
+    cout << setw(13) << left << "PrevBalance"; 
+    cout << setw(13) << left << "Interest"; 
+    cout << setw(13) << left << "Total";
+    cout << setw(13) << left << "Payment";
+    cout << setw(13) << left << "NewBalance";
+    cout << "\n";
+
+    int i = 1;
+    double Interest,total,balance;
+    cout << fixed << setprecision(2); 
+    while (loan > 0.00) 
+    {
+        cout << setw(13) << left << i; 
+        cout << setw(13) << left << loan;
+        Interest = (loan*interest)/100;
+        cout << setw(13) << left << Interest;
+    total = loan + (loan*interest)/100;
+            if (payment > total)
+            {
+                payment = total;
+            }
+        cout << setw(13) << left << total;
+        cout << setw(13) << left << payment;
+        balance = total-payment;
+        cout << setw(13) << left << balance;
+        cout << "\n";
+        i++;
+        loan = balance;
+    }
+
+    return 0;
 }
